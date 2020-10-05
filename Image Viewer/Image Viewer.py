833d3dcb05241f1ea01500814b525e0a7e8c3d2c
woorud <59363022+woorud@users.noreply.github.com>
2020-10-05 23:14:05 +0900

    make image viewer

--- Image Viewer/Image Viewer.py
+++ Image Viewer/Image Viewer.py
@@ -1,11 +1,10 @@
+from PyQt5.QtCore import Qt, QPoint, QRect, QSize
+from PyQt5.QtGui import QImage, QPixmap, QTransform
+from PyQt5.QtWidgets import QMessageBox, QMainWindow, QFileDialog, QApplication, QRubberBand
+from PyQt5.uic import loadUiType
 import sys
 import cv2
 import os
-
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QImage, QPixmap, QTransform
-from PyQt5.QtWidgets import QMessageBox, QMainWindow, QFileDialog, QApplication
-from PyQt5.uic import loadUiType
 
 form_class = loadUiType("Image Viewer.ui")[0]
 valid_format = (".PNG", ".JPEG", ".JPG", ".BMP", ".GIF")
@@ -25,7 +24,14 @@ class ImageViewer(QMainWindow, form_class):
         QMainWindow.__init__(self, parent)
         self.setupUi(self)
 
-        self.scaleFactor = 0.0
+        # origin
+        self.hh = 640
+        self.ww = 640
+        self.idx = 0
+        self.origin = QPoint()
+        self.rubberband = QRubberBand(QRubberBand.Rectangle, self)
+        self.cropEnable = False
+
         # file
         self.a_fileselect.triggered.connect(self.fileselect)
         self.a_folderselect.triggered.connect(self.folderselect)
@@ -37,44 +43,38 @@ class ImageViewer(QMainWindow, form_class):
 
         # edit
         self.a_togray.triggered.connect(self.togray)
-        self.a_zi.triggered.connect(self.zoomin)
-        self.a_zo.triggered.connect(self.zoomout)
         self.a_rotate.triggered.connect(self.rotate)
+        self.a_crop.triggered.connect(self.crop)
+        self.a_original.triggered.connect(self.original)
 
     def fileselect(self):
-        global fileName
         options = QFileDialog.Options()
-        fileName, _ = QFileDialog.getOpenFileName(self,
+        self.fileName, _ = QFileDialog.getOpenFileName(self,
                                                   'Select File',
                                                   '',
                                                   'Images (*.png *.jpeg *.jpg *.bmp *.gif)',
                                                   options=options)
-        if fileName:
-            img = QImage(fileName).scaled(640, 640, Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
-            if img.isNull():
-                QMessageBox.warnig(self, "Image Viewer", "Can't Load %s." % fileName)
-                return
-        self.l_image.setPixmap(QPixmap.fromImage(img))
-        self.scaleFactor = 1.0
+
+        self.img = cv2.imread(self.fileName)
+        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+        self.img_height_origin = self.img.shape[0]
+        self.img_width_origin = self.img.shape[1]
+        # print(self.img_height_origin, self.img_width_origin)
+        self.image2Label(self.img)
         self.b_prev.setEnabled(False)
         self.b_next.setEnabled(False)
 
     def folderselect(self):
-        self.folder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
-        if not self.folder:
-            QMessageBox.warning(self, "Image Viewer", "Can't Load this directory.")
-            return
-
+        self.folder = QFileDialog.getExistingDirectory(self, "Select Directory")
         self.logs = getImgs(self.folder)
         self.numImgs = len(self.logs)
         self.imgs = []
         for file in self.logs:
-            img = QImage(file).scaled(640, 640, Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
-            self.imgs.append(img)
+            self.img = cv2.imread(file)
+            self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+            self.imgs.append(self.img)
 
-        self.idx = 0
-        self.l_image.setPixmap(QPixmap.fromImage(self.imgs[self.idx]))
-        self.scaleFactor = 1.0
+        self.image2Label(self.imgs[self.idx])
 
     def exit(self):
         reply = QMessageBox.question(self,
@@ -90,20 +90,19 @@ class ImageViewer(QMainWindow, form_class):
     def prev(self):
         if self.idx > 0:
             self.idx -= 1
-            self.l_image.setPixmap(QPixmap.fromImage(self.imgs[self.idx]))
+            self.image2Label(self.imgs[self.idx])
         else:
             QMessageBox.warning(self, 'Sorry', 'No more images!')
 
     def next(self):
         if self.idx < self.numImgs-1:
             self.idx += 1
-            self.l_image.setPixmap(QPixmap.fromImage(self.imgs[self.idx]))
+            self.image2Label(self.imgs[self.idx])
         else:
             QMessageBox.warning(self, 'Sorry', 'No more images!')
 
     def togray(self):
-        global fileName
-        img = cv2.imread(fileName)
+        img = cv2.imread(self.fileName)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         r_img = img[:,:,0]
         g_img = img[:,:,1]
@@ -112,27 +111,86 @@ class ImageViewer(QMainWindow, form_class):
         img[:, :, 0] = imgGray
         img[:, :, 1] = imgGray
         img[:, :, 2] = imgGray
-        img = QImage(img, img.shape[1], img.shape[0], img.shape[1]*3, QImage.Format_RGB888).scaled(640, 640,  Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
+        img = QImage(img, img.shape[1], img.shape[0], img.shape[1]*3, QImage.Format_RGB888).scaled(self.hh, self.ww,  aspectRatioMode=True)
         self.qPixmapVar = QPixmap(img)
         self.l_image.setPixmap(self.qPixmapVar)
 
     def rotate(self):
-        global fileName
         angle = 90
-        self.pixmap = QPixmap(fileName).scaled(640, 640, Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
+        self.pixmap = QPixmap(self.fileName).scaled(self.hh, self.ww, aspectRatioMode=True)
         rotated = self.pixmap.transformed(QTransform().rotate(angle))
         self.l_image.setPixmap(rotated)
 
-    def zoomin(self):
-        self.sclaleImage(1.25)
+    def crop(self):
+        self.cropEnable = True
+        self.hw_ratio = self.img_height_origin / self.img_width_origin
+        # print(self.hw_ratio)
 
-    def zoomout(self):
-        self.scaleImage(0.75)
+        if self.hw_ratio > 1: # 세로사진이라면 높이가 최대 길이, 너비가 비례로 축소
+            self.img_height_tran = self.hh
+            self.img_width_tran = int(self.img_height_tran / self.img_height_origin * self.img_width_origin)
+        else: # 가로사진이라면 너비가 최대 너비, 높이가 비례로 축소
+            self.img_width_tran = self.ww
+            self.img_height_tran = int(self.img_width_tran / self.img_width_origin * self.img_height_origin)
+        # print(self.img_height_tran, self.img_width_tran)
 
-    def scaleImage(self, factor):
-        global fileName
-        self.scaleFactor *= factor
+        if self.img_width_tran < self.ww: # 너비가 축소되었다면, 상대 좌표 구하기
+            self.startpos = QPoint((self.ww - self.img_width_tran) // 2, 0)
+            self.endpos = QPoint(self.startpos.x() + self.img_width_tran, self.ww)
+            # print(self.startpos, self.endpos)
+        else: # 높이가 축소되었다면, 상대 좌표 구하기
+            self.startpos = QPoint(0, (self.hh - self.img_height_tran))
+            self.endpos = QPoint(self.hh, self.startpos.y() + self.img_height_tran)
+            # print(self.startpos, self.endpos)
 
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.origin = QPoint(event.pos())
+            self.rubberband.setGeometry(QRect(self.origin, QSize()))
+            self.rubberband.show()
+
+    def mouseMoveEvent(self, event):
+        if not self.origin.isNull():
+            self.rubberband.setGeometry(QRect(self.origin, event.pos()).normalized())
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.rubberband.hide()
+
+        if self.cropEnable == True:
+            self.selStart = self.origin - self.startpos - QPoint(0, 20)
+            self.selEnd = event.pos() - self.startpos - QPoint(0, 20)
+            # print(self.selStart, self.selEnd)
+
+            cut_begin_x = int(self.img_width_origin * self.selStart.x() / self.img_width_tran)
+            cut_begin_y = int(self.img_height_origin * self.selStart.y() / self.img_height_tran)
+            cut_end_x = int(self.img_width_origin * self.selEnd.x() / self.img_width_tran)
+            cut_end_y = int(self.img_height_origin * self.selEnd.y() / self.img_height_tran)
+            # print(cut_begin_x, cut_begin_y, cut_end_x, cut_end_y)
+
+            self.img = cv2.imread(self.fileName)
+            self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+            self.img = self.img[cut_begin_y:cut_end_y, cut_begin_x:cut_end_x, :].astype("uint8")
+            self.image2Label(self.img)
+            self.cropEnable = False
+
+    def image2Label(self, img):
+        self.qPixmapVar = QPixmap(self.image2QImage(img))
+        self.qPixmapVar = self.qPixmapVar.scaled(self.hh, self.ww, aspectRatioMode=True)
+        self.l_image.setPixmap(self.qPixmapVar)
+
+    def file2QImage(self, fileName):
+        img = cv2.imread(fileName)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return QImage(img, img.shape[1], img.shape[0], img.shape[1]*3, QImage.Format_RGB888)
+
+    def image2QImage(self, img):
+        return QImage(img, img.shape[1], img.shape[0], img.shape[1]*3, QImage.Format_RGB888)
+
+    def original(self):
+        self.img = cv2.imread(self.fileName)
+        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+        self.image2Label(self.img)
 
 app = QApplication(sys.argv)
 my_window = ImageViewer(None)
